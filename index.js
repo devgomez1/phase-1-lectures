c5dec5b825ebcd89e438a63615e23d418c2cdbe5
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-10-30 15:54:59 -0500

    populated index.js

---
+++ index.js
@@ -0,0 +1,71 @@
+// Deliverables
+
+// JavaScript Fundamentals and Data Structures Workshop
+
+// Workshop Introduction:
+// In this workshop, we'll cover JavaScript fundamentals and data structures to help you build a strong foundation.
+// Let's get started!
+
+// Variables and Data Types
+
+// 1. Declare a variable 'age' and assign your age to it.
+// 2. Declare a constant 'name' and assign your name to it.
+// 3. Declare a variable 'isStudent' and assign a boolean value to it.
+// 4. Declare a variable 'score' and assign a numeric value to it.
+// 5. Declare an array 'fruits' and add a few different fruit names to it.
+// 6. Declare an object 'person' with properties 'name' and 'age'.
+
+// Console Output
+
+// 7. Use 'console.log' to display a welcome message to the console.
+
+// Math Operations
+
+// 8. Perform addition and multiplication operations on two numbers and store the results in variables.
+
+// Strings
+
+// 9. Create a string variable 'greeting'
+// 10. Use string interpolation to create a message by combining 'greeting' and 'name'.
+
+// Conditional Statements
+
+// 11. Write an 'if' statement to check if your age is greater than or equal to 18.
+
+// 12. Use 'if-else' to display different messages based on isStudent value.
+// a. If isStudent is true, display 'Welcome to Flatiron!'
+// b. If isStudent is false, display 'Welcome!'
+
+// Switch Statements
+
+// Use a switch statement to display different messages based on the value of 'score'.
+
+// Ternary Operator
+
+// Use a ternary operator to display different messages based on the value of 'score'.
+
+// Loops
+
+// 13. Use a 'for' loop to print numbers from 1 to 5.
+// 14. Use a 'while' loop to print numbers from 1 to 5.
+
+// Arrays
+
+// 15. Declare an array 'colors' with a few color names.
+
+// Loop through the 'colors' array and print each color to the console.
+// Loop through the 'colors' array and print the index of each color to the console.
+
+// ! difference between 'for', 'for...of', and 'for...in' loops
+
+// 16. Add a new color to the 'colors' array.
+
+// 17. Access the first and last color in the array.
+
+// access the color at a specific index.
+
+// find the index of a color.
+
+// check if a color exists in the 'colors' array.
+// remove the last color from the 'colors' array.
+// remove the first color from the 'colors' array.
